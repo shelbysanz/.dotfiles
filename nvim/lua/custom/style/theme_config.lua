4f11a51542da981bfa1315d48267e4df1844f3cd
Shelby Sanchez <shelby@bluearrowmail.com>
2025-02-28 16:03:50 -0500

    style: removing customer comment colors

--- nvim/lua/custom/style/theme_config.lua
+++ nvim/lua/custom/style/theme_config.lua
@@ -8,12 +8,6 @@ return {
         sidebars = 'transparent',
         floats = 'transparent',
       },
-      on_highlights = function(hl)
-        hl.comment = { fg = '#6A9955' }
-        hl.Comment = { fg = '#6A9955' }
-        hl.DiagnosticUnnecessary = { fg = '#717171' }
-        hl.LspDiagnosticsUnnecessary = { fg = '#717171' }
-      end,
     },
   },
   {
